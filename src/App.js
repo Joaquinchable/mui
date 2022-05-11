62c598201762f867b48e601165cae11433a9ae10
Joaquin Alberto Chable Rodriguez <joaquin@alimx.mx>
2022-05-11 15:20:43 -0500

    Mui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+// import GridResposive from './Components/Grid';
+import Forma from './Components/Forma';
+import Header from './Components/Header';
+import Container from '@material-ui/core/Container';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header/>
+        <Container>
+          <Forma/>
+        </Container>
     </div>
   );
 }
